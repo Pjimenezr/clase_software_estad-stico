0526790a084565fa3ea32c212c1b3f9dca194d7b
Nokinigth <bastyanvera2006@gmail.com>
2025-11-28 16:33:12 -0300

    Puse las funciones, pero la 3ra da error, hay que verla

---
+++ Evaluación_unidad_3/S_Osses__B_Vera/main.R
@@ -0,0 +1,10 @@
+library(stringr)
+library(purrr)
+library(dplyr)
+
+urls <- c("https://www.ine.gob.cl/docs/default-source/encuesta-suplementaria-de-ingresos/bbdd/csv_esi/2022/esi_2022.csv?sfvrsn=9c939ccc_4&download=true",
+          "https://www.ine.gob.cl/docs/default-source/encuesta-suplementaria-de-ingresos/bbdd/csv_esi/2021/esi_2021.csv?sfvrsn=bead03fb_4&download=true",
+          "https://www.ine.gob.cl/docs/default-source/encuesta-suplementaria-de-ingresos/bbdd/csv_esi/2020/esi-2020-personas.csv?sfvrsn=66c7160_4&download=true",
+          "https://www.ine.gob.cl/docs/default-source/encuesta-suplementaria-de-ingresos/bbdd/csv_esi/2019/esi-2019-personas.csv?sfvrsn=7d8832d9_4&download=true",
+          "https://www.ine.gob.cl/docs/default-source/encuesta-suplementaria-de-ingresos/bbdd/csv_esi/2018/esi-2018-personas.csv?sfvrsn=8a269cc6_4&download=true"
+)
