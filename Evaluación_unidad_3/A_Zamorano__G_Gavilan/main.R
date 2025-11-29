c36c42c045644de9a9cef0697973d59115af9729
GGavilan07 <gavilangabriel6@gmail.com>
2025-11-29 12:08:18 -0300

    terminalo aikdo

--- Evaluación_unidad_3/A_Zamorano__G_Gavilan/main.R
+++ Evaluación_unidad_3/A_Zamorano__G_Gavilan/main.R
@@ -5,4 +5,15 @@ library(data.table)
 library(microbenchmark)
 library(stringr)
 
-source("funciones.R")
+source("functions.R")
+###cabe aclarar que la "version" que aparecen en nuestras tablas resumen corresponden a la posición del los urls que pertenecen al vector url
+### por ejemplo, version 1 es la ESI del año 2018, version 2 es la ESI del 2019 y así sucesivamente.
+# tabla de personas y hogares ---------------------------------------------
+tabla_personas_hogares <- map_df(lista_bases, personas_hogar, .id ="version")
+print(tabla_personas_hogares)
+
+
+# tabla estadísticos de ingresos del trabajo principal --------------------
+
+tabla_ingresos_principal<-map_df(lista_bases, estadisticos_ingresos, .id ="version" )
+print(tabla_ingresos_principal)
