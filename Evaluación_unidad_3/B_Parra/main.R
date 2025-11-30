a889e83df02d73dcfef80a66c1f65d8f050c078b
Aunstrums <bastyan.parra.santander@gmail.com>
2025-11-30 16:42:44 -0300

    Inicio del script main
    - se carga el archivo functions.r y se verifica que las librerías estén cargadas correctamente

--- Evaluación_unidad_3/B_Parra/main.R
+++ Evaluación_unidad_3/B_Parra/main.R
@@ -1,1 +1,9 @@
-###
+### En este va el script principal, el que ejecuta el flujo como tal.
+
+rm(list = ls())
+source("functions.R")
+
+cat("Librerías cargadas correctamente\n")
+cat("Versión de R:", R.version.string, "\n\n")
+
+
