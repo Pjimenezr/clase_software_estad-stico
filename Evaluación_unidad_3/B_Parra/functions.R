1dbf9caf8e706565e8c2a8a9af37d5bfb4d4afd1
Aunstrums <bastyan.parra.santander@gmail.com>
2025-11-29 21:12:54 -0300

    Empiezo cargando las librerías

--- Evaluación_unidad_3/B_Parra/functions.R
+++ Evaluación_unidad_3/B_Parra/functions.R
@@ -1,1 +1,11 @@
 ###
+
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("purrr")) install.packages("purrr")
+if (!require("data.table")) install.packages("data.table")
+if (!require("microbenchmark")) install.packages("microbenchmark")
+
+library(dplyr)
+library(purrr)
+library(data.table)
+library(microbenchmark)
