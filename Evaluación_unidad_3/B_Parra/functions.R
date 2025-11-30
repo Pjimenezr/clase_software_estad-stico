9b4a7502afefe3fc45f0356844363c30b687ec6c
Aunstrums <bastyan.parra.santander@gmail.com>
2025-11-30 16:00:10 -0300

    Se crearon las funciones que implementan los indicadores solicitados.
    Aquí va tanto la función que a partir de los datos entrega una tabla con las personas y hogares según el año de los datos, como la función que va a entregar la tabla con los estadísticos que se piden para la variable int_t_p, habiendo filtrado los datos con la condición ocup_rep==1

--- Evaluación_unidad_3/B_Parra/functions.R
+++ Evaluación_unidad_3/B_Parra/functions.R
@@ -13,7 +13,7 @@ library(microbenchmark)
 
 
 #### Ejercicio 1 (apartado número 3 de las instrucciones)
-### 1.1
+### 3.1
 extract_name <- function(url) {
   nombre <- sub(".*/([^/]+\\.csv).*", "\\1", url)
   return(nombre)
@@ -39,7 +39,8 @@ tryCatch({
 
 
 #### Ejercicio 2 (es el número 4 en la pauta de instrucciones) #######
-### lo de la función de descarga masiva lo he puesto (punto 3,3 de la pauta) en el main profe
+### lo de la función de descarga masiva lo he puesto (punto 3.3 de la pauta) en el main profe
+## 4.1
 
 read_esi_data <- function(path) {
   primera_linea <- readLines(path, n = 1, warn = FALSE)
@@ -62,8 +63,42 @@ read_esi_data <- function(path) {
   return(datos)
 }
 
+### igual que antes, el apartado 4.2 (lo de cargar las bases en una lista) lo he puesto en el main
 
+##### Ejercicio 3: hacer las tablas e indicadores
+### 5.1
 
+crear_tabla_personas_hogares <- function(lista_datos, nombres_archivos) {
+  # Usa map2_df para iterar sobre datos y nombres simultáneamente
+  resultados <- map2_df(lista_datos, nombres_archivos, function(datos, nombre) {
+    # Crea un tibble (tabla moderna) con los resultados
+    tibble(
+      version = nombre,  # Nombre del archivo
+      num_personas = n_distinct(datos$idrph),  # Personas únicas
+      num_hogares = n_distinct(datos$id_identificacion)  # Hogares únicos
+    )
+  })
+  
+  return(resultados)
+}
+
+## 5.2
+crear_tabla_ingresos <- function(lista_datos, nombres_archivos) {
+  resultados <- map2_df(lista_datos, nombres_archivos, function(datos, nombre) {
+    datos_ocupados <- datos %>% 
+      filter(ocup_ref == 1)
+    tibble(
+      version = nombre,
+      minimo = min(datos_ocupados$ing_t_p, na.rm = TRUE),
+      maximo = max(datos_ocupados$ing_t_p, na.rm = TRUE),
+      media = mean(datos_ocupados$ing_t_p, na.rm = TRUE),
+      percentil_10 = quantile(datos_ocupados$ing_t_p, 0.10, na.rm = TRUE),
+      percentil_90 = quantile(datos_ocupados$ing_t_p, 0.90, na.rm = TRUE)
+    )
+  })
+  
+  return(resultados)
+}
 
 
 
